31bab2e2d23b662917e29bc515152e40f3625c97
git <eae966@gmail.com>
2023-08-28 02:44:14 +0330

    ثقفثقفثقف

--- src/exchange/factory.ts
+++ src/exchange/factory.ts
@@ -41,7 +41,7 @@ export function handlePairCreated(event: PairCreated): void {
   if (token0 === null) {
     token0 = new Token(event.params.token0.toHexString());
     log.info("try to Token 0 is {}" , [event.params.token0.toHexString()])
-    if (event.params.token0.toHexString() === '0x30199Be78D0A2A885b3E03f7D5B08DE2ad251648') {
+    if (event.params.token0.toHexString().toUpperCase() === '0x30199BE78D0A2A885B3E03F7D5B08DE2AD251648') {
       token0.symbol = 'CASHUSD';
       token0.name = 'CashUSD';
       token0.totalSupply = BigInt.fromString('10000000000');
