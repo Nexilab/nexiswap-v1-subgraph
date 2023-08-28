b57f697a67c0557eb4c818ac7265384c5dac2e73
git <eae966@gmail.com>
2023-08-28 13:46:26 +0330

    Some Updates.

--- src/exchange/factory.ts
+++ src/exchange/factory.ts
@@ -123,6 +123,9 @@ export function handlePairCreated(event: PairCreated): void {
   // fetch info if null
   if (token0 === null) {
     token0 = new Token(event.params.token0.toHexString());
+    if (event.params.token0.toHexString() === '0x30199Be78D0A2A885b3E03f7D5B08DE2ad251648') {
+
+    }
     token0.symbol = fetchTokenSymbol(event.params.token0);
     token0.name = fetchTokenName(event.params.token0);
     token0.totalSupply = fetchTokenTotalSupply(event.params.token0);
